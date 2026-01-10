04fa988e9a6457e959f3eb5f5221aa1c3daba4ba
Sourav Mandal <sm227465@gmail.com>
2026-01-10 19:34:16 +0530

    fix: gradlew error

--- android/app/src/main/java/com/anonymous/melodify/MainActivity.kt
+++ android/app/src/main/java/com/anonymous/melodify/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.musicapp
+package com.anonymous.melodify
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/anonymous/melodify/MainApplication.kt
+++ android/app/src/main/java/com/anonymous/melodify/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.musicapp
+package com.anonymous.melodify
 
 import android.app.Application
 import android.content.res.Configuration
